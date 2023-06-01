dee4dd75a1c2c4186017aa6be085758b9b1ececd
Amartya1110 <amartyakhan999@gmail.com>
2023-06-01 16:33:11 +0530

    CORS issue handled in ExpressJs Server

--- server/index.js
+++ server/index.js
@@ -15,8 +15,17 @@ connectToDB()
 const app = express();
 
 // App level middleware
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:1234"); // update to match the domain you will make the request from
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader ('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
 
 app.use("/todo", todoRoutes)
 
